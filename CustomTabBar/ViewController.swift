c14beeb7a7195a3463900075e4ee7e84e7d0707a
grinya37 <112849355+grinya37@users.noreply.github.com>
2023-10-06 17:46:48 +0300

    add_project.Delete_storyboard

---
+++ CustomTabBar/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CustomTabBar
+//
+//  Created by Николай Гринько on 06.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        view.backgroundColor = .gray
+      
+    }
+
+
+}
+
